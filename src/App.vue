a31d0e078c6f6336978758b51ae961ead9bd6b69
mukhulaazam <mukhulaazam@gmail.com>
2022-06-15 01:23:31 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Nav from './components/Nav.vue'
+import About from './components/About.vue'
+import Projects from './components/Projects.vue'
+import Feature from './components/Fearure.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+      <Nav />
+    </header>
+    <section>
+      <About/>
+    </section>
+    <section>
+      <Projects/>
+    </section>
+    <section>
+      <Feature/>
+    </section>
+  </div>
+</template>
+
+<style>
+@import '@/assets/css/bootstrap.min.css';
+@import '@/assets/css/unicons.css';
+@import '@/assets/css/owl.carousel.min.css';
+@import '@/assets/css/owl.theme.default.min.css';
+
+
+@import '@/assets/css/tooplate-style.css';
+
+</style>
